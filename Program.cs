445018a6048f1114d2b623fa0e89ef3dbe5473cc
chasgabster <gabriella.iofe@chasacademy.se>
2025-09-10 00:17:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_repo_VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
